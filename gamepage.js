f1464d5edb5e68d3f63f817c3949a60698f3b42c
Fischl-birb <101027023+Fischl-birb@users.noreply.github.com>
2022-08-04 19:21:36 -0400

    Add files via upload

--- gamepage.js
+++ gamepage.js
@@ -1,12 +1,14 @@
+var guess = 1;
+var y = Math.floor(Math.random()* 10 + 1);
+playername= localStorage.getItem("player_name")
 function submit(){
 // random value generated
-  var y = Math.floor(Math.random()* 10 + 1);
 // counting the number of guesses
 var x = document.getElementById("guessField").value;
 // made for correct Guess
 if(x==y)
 {
-alert("CONGRATULATIONSS!!"+playername+"YOU GUESSED IT IN"+guess+"GUESS(ES)");
+alert("CONGRATULATIONSS!!"+ playername +"YOU GUESSED IT IN"+  guess  +"GUESS(ES)");
 guess=1;
 }
 else if(x > y) /* if guessed number is greater than actual number*/
